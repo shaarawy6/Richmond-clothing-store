6f29204c8eb6144f3e6e3de75faa61f23b0dc6da
shaarawy6 <mohamedelsharawy6666@gmail.com>
2024-06-29 17:38:56 +0300

    update

--- JS/view-bar.js
+++ JS/view-bar.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.querySelector('.menu-icon');
-    const navLinks = document.querySelector('.nav__links');
+    const navLinks = document.querySelector('.nav__links2');
 
     menuIcon.addEventListener('click', function() {
         navLinks.classList.toggle('active');
